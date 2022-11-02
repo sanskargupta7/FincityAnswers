7e6dd527e2dbbf0ef67a05e957cccf94df435c83
Sanskar Gupta <72748648+sanskargupta7@users.noreply.github.com>
2022-11-03 03:53:28 +0530

    Q5

---
+++ SupermanAndBottledCityOfKandor.java
@@ -0,0 +1,58 @@
+import java.io.*;
+
+//Both the functions return the possible correct answer
+
+class SupermanAndBottleCityOfKandor {
+    
+    //This function prints all the places where people can live
+    /*public static void findPlanet(double[][][] a, double[] b){
+        double min = b[0];
+        double max = b[1];
+        double sum;
+        
+        for(int i=0; i<a.length; i++){
+            
+            sum = 0;
+        
+            for(int j=0; j<a[i].length;j++){
+                sum += a[i][j][0]/a[i][j][1];
+            }
+            
+            if(sum<=max && sum>=min){
+                System.out.println(i);
+            }
+        }
+    }*/
+    
+    
+    //This function returns the first habitable planet 
+    public static int fun(double[][][] a, double[] b){
+        double min = b[0];
+        double max = b[1];
+        double sum;
+        
+        for(int i=0; i<a.length; i++){
+            
+            sum = 0;
+        
+            for(int j=0; j<a[i].length;j++){
+                sum += a[i][j][0]/a[i][j][1];
+            }
+            
+            if(sum<=max && sum>=min){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+    
+	public static void main (String[] args) {
+		
+		double[][][] a = {{{0.433, 200}}, {{0.89, 400}, {0.6, 300}}};
+		double[] b = {0.003, 0.005};
+		
+		System.out.println(fun(a, b));
+		
+	}
+}
