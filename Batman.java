6fc4357e8eac7f6bfc717452536c9f6e46a70800
Sanskar Gupta <72748648+sanskargupta7@users.noreply.github.com>
2022-11-03 03:57:06 +0530

    Q2

---
+++ Batman.java
@@ -0,0 +1,15 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class BATMAN {
+    
+    public static int findNumOfStepsRequired(int np, int ps1){
+        return np*ps1;
+    }
+    
+	public static void main (String[] args) {
+		System.out.println(findNumOfStepsRequired(3, 10));
+		
+	}
+}
