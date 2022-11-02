d3b690a87c3327ccb13f46c3a7efea5314a1bd9f
Sanskar Gupta <72748648+sanskargupta7@users.noreply.github.com>
2022-11-03 04:27:08 +0530

    Q2

--- Batman.java
+++ Batman.java
@@ -5,7 +5,7 @@ class BATMAN {
     public static int findNumOfStepsRequired(int np, int ps1){
         return np*ps1;
     }
-    
+     
 	public static void main (String[] args) {
 		System.out.println(findNumOfStepsRequired(3, 10));
 		
